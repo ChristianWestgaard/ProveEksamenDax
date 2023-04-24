5b7dfc75c3169a159264cc5ee94d5b5bd1abb3e8
Christian Westgaard <westgaard04@gmail.com>
2023-04-24 10:44:55 +0200

    DataFix

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ function saveData(){
 
 
 
-
+const { AsyncLocalStorage } = require('async_hooks');
+const { log } = require('console');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://christianwestgaard04:Admin1@visitorname.t4qyvrp.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://user1:Password1@visitorname.t4qyvrp.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,14 +26,34 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await listDatabases(client);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+
+async function listDB(client) {
+    const databasesList = await client.db().admin().listDB();
+
+    console.log("Databases");
+    databasesList.databases.forEach(db => {
+        console.log("- ${db.name}");
+    })
+} 
+
+// async function checklist() {
+//   try {
+
+//     await client.connect();
+
+//     if (localStorage.getItem("DittNavn") == document.getElementById("saveNavn").value) {
+//         visitorList.innerHTML =
+//     }
+
+//   } finally {
+
+//     await client.close();
+//   }
+// }
+
